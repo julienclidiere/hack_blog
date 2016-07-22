1221f46c3ff133cc55f6024bca7a0f3442524eb9
Luke Rickard <luke.x.rickard@gmail.com>
2016-07-22 16:40:57 +0100

    fixing issue with mysql deployment

--- settings/prod.py
+++ settings/prod.py
@@ -1,10 +1,17 @@
 from base import *
 import dj_database_url
 
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES = { 'default': {} }
 
-DATABASES['default'] = dj_database_url.parse("mysql://bec999e1787895:978e9893@eu-cdbr-west-01.cleardb.com/heroku_9b53a7d93e26e04?reconnect=true")
+DATABASES['default'] = dj_database_url.parse(
+    "mysql://bec999e1787895:978e9893@eu-cdbr-west-01.cleardb.com/heroku_9b53a7d93e26e04")
+
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
